5ebdd1e11ee007976f283af7a118c990592a9b50
kanhaiya-bhayana-inc <kanhaiya.bhayana@incedoinc.com>
2023-11-24 18:53:20 +0530

    Updated

---
+++ src/Components/APIs/ADFPipeLine/api.js
@@ -0,0 +1,16 @@
+export async function fetchADFPipelineData(){
+    try{
+        const response = await fetch('https://cloudfmsadfpipeline.azurewebsites.net/api/Pipeline',{
+            method: 'GET',
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "POST, GET, OPTIONS, DELETE",
+            },
+        });
+        const data = await response.json();
+        return await data;
+    } catch(error){
+        console.log('Error fetching pipelines data:', error);
+        return [];
+    }
+}
--- src/Components/Test/Testing.js
+++ src/Components/Test/Testing.js
@@ -1,75 +1,79 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import SweetPagination from "sweetpagination";
+import { fetchADFPipelineData } from '../APIs/ADFPipeLine/api';
+import loadingBar from '../../images/microsoftLoadingBar.gif';
+import styles from './Testing.module.css';
 
 
 export default function Testing() {
   const [currentPageData, setCurrentPageData] = useState(new Array(2).fill());
+  const [pipelinesDataList, setPipelinesDataList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchADFPipelineData();
+        setPipelinesDataList(data);
+        console.log(data); // Log the data after setting the state
+      } catch (error) {
+        console.error('Error setting pipeline data options:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   // Example items, to simulate fetching from another resources.
   const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
   return (
-    <div>
-      {/* <table class="table">
-        <thead class="thead-light">
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-      </table> */}
-      <div className='p-3' id='mydiv'>
-        <table class="table">
-          <thead class="thead-light">
-            <tr>
-              <th scope="col">Id</th>
-              <th scope="col">Name</th>
-              <th scope="col">Status</th>
-              <th scope="col">Action</th>
-
-            </tr>
-          </thead>
-          <tbody>
-            {currentPageData.map((item) => (
+    <div className='container'>
+      {pipelinesDataList.length != 0 ?
+        <div className='p-3' id='mydiv'>
+          <table class="table table-bordered">
+            <thead>
               <tr>
-                <td>Larry {item}</td>
-                <td>the Bird {item}</td>
-                <td>@twitter {item}</td>
-                <td><button style={{border:"none",background:"none"}}><i class="bi bi-arrow-clockwise"></i></button></td>
+                <th scope="col">Pipeline name</th>
+                <th scope="col">Run start</th>
+                <th scope="col">Run end</th>
+                <th scope="col">Duration</th>
+                <th scope="col">Status</th>
+                <th scope="col">Error</th>
+                <th scope="col">Parameters</th>
+                <th scope="col">Run Id</th>
+                <th scope="col">Action</th>
+
               </tr>
-            ))}
+            </thead>
+            <tbody>
+              {pipelinesDataList && pipelinesDataList.map((data, i) => (
+                <tr key={i}>
+                  <td>{data.pipelineName}</td>
+                  <td>{data.runStart}</td>
+                  <td>{data.runEnd}</td>
+                  <td>{data.durationInMS}</td>
+                  <td>{data.status}</td>
+                  <td>{data.errorMessage != "" ?<div style={{color:"red"}} className={styles.errorCell}>
+                    <span className={styles.visibleContent}>{data.errorMessage.split(' ').slice(0, 4).join(' ')}</span>
+                    <span className={styles.fullContent}>{data.errorMessage}</span>
+                  </div>:"---"}</td>
+                  <td>TBD</td>
+                  <td>{data.runID}</td>
+                  <td><button style={{ border: "none", background: "none" }}><i class="bi bi-arrow-clockwise"></i></button></td>
+                </tr>
+              ))}
 
-          </tbody>
 
-        </table>
-        <SweetPagination
-          currentPageData={setCurrentPageData}
-          getData={items}
-          dataPerPage={10}
-          navigation={true}
-          getStyle={'style-1'}
-        />
-      </div>
+            </tbody>
+
+          </table>
+          <SweetPagination
+            currentPageData={setCurrentPageData}
+            getData={pipelinesDataList}
+            dataPerPage={10}
+            navigation={true}
+            getStyle={'style-1'}
+          />
+        </div> : <div className='p-5 mt-5'><img src={loadingBar} className='mt-5' height={100} /></div>}
     </div>
   );
 }
